2e3c3cbf4a1f7a2edf5550d7c36ce4ff6c6f042b
Dianza <dianabondarenko1321@gmail.com>
2023-11-21 22:07:30 +0200

    MA1-11: added function to sort array in ascending and descending order

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [selectValue, setSelectValue] = useState('maxValue');
   const [answer, setAnswer] = useState('');
   const [inputValue, setInputValue] = useState('')
+  const [sortedArray, setSortedArray] = useState([]);
 
   const chooseFormula = ( data ) => {
 
@@ -22,9 +23,24 @@ function App() {
     }    
   }
 
+  const sortAndDisplay = () => {
+    const sortedData = inputValue
+      .split(/[\s,]+/)
+      .filter((i) => i !== '-' && i !== '')
+      .map((i) => +i)
+      .sort((a, b) => (selectValue === 'asc' ? a - b : b - a));
+
+    setSortedArray(sortedData);
+  };
+
+
   useEffect(()=>{
-    chooseFormula(inputValue.split(',').filter(i => i !== '-' & i !== '').map(i => +i))
+    chooseFormula(inputValue.split(',').filter((i) => i !== '-' && i !== '').map((i) => +i))
   }, [selectValue, inputValue])
+
+  useEffect(() => {
+    sortAndDisplay();
+  }, [selectValue])
 
   return (
     <div className="mainContainer">
@@ -44,12 +60,30 @@ function App() {
           >
             <option value="maxValue">Максимальне значення</option>
             <option value="minValue">Мінімальне значення</option>
+            <option value="asc">Сортування за зростанням</option>
+            <option value="desc">Сортування за спаданням</option>
           </select>
         </div>  
-        <div>
+        
+
+        <div className="numberContainer">
+          {selectValue === 'asc' || selectValue === 'desc' ? (
+            <table>
+              <tbody>
+                <tr>
+                  {sortedArray.map((number, index) => (
+                    <td key={index}>{number}</td>
+                 
+                ))}
+                </tr>
+              </tbody>
+            </table>
+          ) : (
+
           <span>
             {answer}
           </span>
+          )}
         </div>
       </div>
     </div>
